4b5254804f6f7f12ed2cdb204f96078f9a204690
saurav <sauravswain12345@gmail.com>
2025-08-04 12:04:53 +0530

    --apiContext change infrontend--

--- app/src/apis/apiConstants.ts
+++ app/src/apis/apiConstants.ts
@@ -1,34 +1,17 @@
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? '';
-
-// export const AUTH_APIS = {
-//   LOGIN: '/auth/Login',
-//   STUDENT_REGISTER: '/auth/StudentRegister',
-//   COMPANY_REGISTER: '/auth/CompanyRegister',
-//   MENTOR_REGISTER: '/auth/MentorRegister',
-//   RESEND_OTP: '/auth/ResendOTP',
-//   VERIFY_OTP: '/auth/VerifyRegisterOTP',
-//   FORGOT_PASSWORD: '/auth/SendForgotPasswordMail',
-//   VERIFY_FORGOT_PASSWORD: '/auth/VerifyForgotPasswordOTP',
-//   RESET_PASSWORD: '/auth/ResetPassword',
-//   LOG_OUT: '/auth/logout',
-//   VERIFY_ME: '/auth/me',
-// };
-
-
-
-export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? '';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://careerglide.onrender.com/api';
 
 export const AUTH_APIS = {
-  LOGIN: `${API_URL}/auth/Login`,
-  STUDENT_REGISTER: `${API_URL}/auth/StudentRegister`,
-  COMPANY_REGISTER: `${API_URL}/auth/CompanyRegister`,
-  MENTOR_REGISTER: `${API_URL}/auth/MentorRegister`,
-  RESEND_OTP: `${API_URL}/auth/ResendOTP`,
-  VERIFY_OTP: `${API_URL}/auth/VerifyRegisterOTP`,
-  FORGOT_PASSWORD: `${API_URL}/auth/SendForgotPasswordMail`,
-  VERIFY_FORGOT_PASSWORD: `${API_URL}/auth/VerifyForgotPasswordOTP`,
-  RESET_PASSWORD: `${API_URL}/auth/ResetPassword`,
-  LOG_OUT: `${API_URL}/auth/logout`,
-  VERIFY_ME: `${API_URL}/auth/me`,
+  LOGIN: '/auth/Login',
+  STUDENT_REGISTER: '/auth/StudentRegister',
+  COMPANY_REGISTER: '/auth/CompanyRegister',
+  MENTOR_REGISTER: '/auth/MentorRegister',
+  RESEND_OTP: '/auth/ResendOTP',
+  VERIFY_OTP: '/auth/VerifyRegisterOTP',
+  FORGOT_PASSWORD: '/auth/SendForgotPasswordMail',
+  VERIFY_FORGOT_PASSWORD: '/auth/VerifyForgotPasswordOTP',
+  RESET_PASSWORD: '/auth/ResetPassword',
+  LOG_OUT: '/auth/logout',
+  VERIFY_ME: '/auth/me',
 };
+
 
